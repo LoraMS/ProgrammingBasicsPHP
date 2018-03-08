d58e28e675353bfb7eed63082c225ec3da5cc270
LoraMS <lorashopova@abv.bg>
2018-03-09 01:34:06 +0200

    Exam Preparation
    Exam Preparation

--- ExamPreparation/Exam06-03-2016/on-time-for-the-exam.php
+++ ExamPreparation/Exam06-03-2016/on-time-for-the-exam.php
@@ -46,3 +46,48 @@ if ($examTime > $arrivalTime) {
 } else {
     echo "On time";
 }
+
+/*
+$hoursStart = intval(readline());
+$minutesStart = intval(readline());
+$hoursArrive = intval(readline());
+$minutesArrive = intval(readline());
+
+$start = $hoursStart * 60 + $minutesStart;
+$arrive = $hoursArrive * 60 + $minutesArrive;
+
+if($start === $arrive){
+    echo "On time\n";
+} elseif ($start - $arrive > 0 && $start - $arrive <= 30) {
+    echo "On time\n";
+    echo ($start - $arrive)." minutes before the start";
+} elseif ($start - $arrive > 30) {
+    echo "Early\n";
+    if ($start - $arrive < 60) {
+        echo ($start - $arrive)." minutes before the start";
+    } else {
+        $early = $start - $arrive;
+        $hh = floor($early/60);
+        $mm = $early % 60;
+        echo $hh.":";
+        if($mm < 10){
+            echo "0";
+        }
+        echo "$mm hours before the start";
+    }
+} elseif ($arrive > $start) {
+    echo "Late\n";
+    if ($arrive - $start <= 59) {
+        echo $arrive - $start." minutes after the start";
+    } else{
+        $late = $arrive - $start;
+        $hh = floor($late/60);
+        $mm = $late % 60;
+        echo $hh.":";
+        if($mm < 10){
+            echo "0";
+        }
+        echo "$mm hours after the start";
+    }
+}
+*/
